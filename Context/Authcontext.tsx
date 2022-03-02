91437e73f7daaed1f90d3df1fbd88d396fee013d
felixstenian <felixstenian@gmail.com>
2022-03-02 20:41:07 -0300

    Salvando token nos cookies && recuperando estado da autenticação

--- Context/Authcontext.tsx
+++ Context/Authcontext.tsx
@@ -1,11 +1,18 @@
 import Router from "next/router";
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { 
+  setCookie, // armazena o cokkie
+  parseCookies  // retorna uma lista com todos os cookies armazenados
+} from 'nookies'
+
 import { api } from "../services/api";
 
 type User = {
   email: string;
   permissions: String[];
   roles: String[];
+  token?: string;
+  refreshToken?: string;
 }
 
 type SignInCredentials = {
@@ -28,17 +35,38 @@ export const AuthContext = createContext({} as AuthContextData)
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
-  
+
+  // Fazer com que toda vez que o usuário acessar a aplicação apenas pela primeira vez, carregar os dados de usuário novamente
+  useEffect(() => {
+    const { 'nextauth.token': token  } = parseCookies()
+
+    if (token)
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({ email, permissions, roles })
+      })
+  }, [])
+
   const signIn = async ({ email, password }: SignInCredentials) => {
     try {
       const { 
-        data: { permissions, roles } 
+        data: { permissions, roles, token, refreshToken } 
       } = await api.post('sessions', {
         email,
         password
       })
 
-      setUser({ email, permissions, roles })
+      setUser({ email, permissions, roles, token, refreshToken })
+      
+      setCookie(undefined, 'nextauth.token', token, {  // recebe 3 parametros obrigatorios e também conseguimos passar algumas configurações / 1. contexto da requisição (mas como o usuário realiza o login pelo navegador, não existe requisição, então envamos como undefined) / 2. nome do cookie / 3. valor do token
+        maxAge: 60 * 60 * 24 * 30, // 30 days // Tempo em que o cookie fica salvo no navegador
+        path: '/' // diz quais caminhos da aplicação vão ter acesso a esse cookie, e quando colocamos '/' falamos que qualquer endereço da aplicação pode ter acesso ao cookie
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: '/'
+      })
       Router.push('/dashboard')
       
     } catch (error) {
@@ -51,3 +79,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     </AuthContext.Provider>
   )
 }
+
+// diego@rocketseat.team
+// 123456
