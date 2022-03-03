9c21819ed4a5b0dc8c2f625fb4a810ebfdc4c44e
felixstenian <felixstenian@gmail.com>
2022-03-02 21:07:51 -0300

    Realizando refresh do token

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,42 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
   }
+})
+
+api.interceptors.request.use(response => { // Recebe dois parametros. 1. resposta que deu sucesso e retornamos sem fazer nada; 2. se deu error
+  return response
+}, (error: AxiosError) => {
+  if (error.response?.status === 401)
+    if (error.response.data?.code === 'token.expired') {
+      // renova token
+      cookies = parseCookies()
+
+      const { 'next.refreshToken': refreshToken } = cookies
+
+      api.post('/refresh', {
+        refreshToken,
+      }).then(response => {
+        const { token, refreshToken: newRefreshToken } = response.data
+
+        setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+        setCookie(undefined, 'nextauth.refreshToken', newRefreshToken, {
+          maxAge: 60 * 60 * 24 * 30,
+          path: '/'
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      })
+    } else {
+      // desloga usuário
+    }
 })
