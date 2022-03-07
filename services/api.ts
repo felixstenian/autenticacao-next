3d0b7bee516d64146e1576817097960ee2e4839d
felixstenian <felixstenian@gmail.com>
2022-03-07 19:16:03 -0300

    Fila de requisições no Axios

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
 
 let cookies = parseCookies()
+let isRefreshig = false
+let filedRequestsQueue: any = [] // fila com todas as requisições que aconteceram e deram falha por conta do tokem expirado
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -19,22 +21,50 @@ api.interceptors.request.use(response => { // Recebe dois parametros. 1. respost
       cookies = parseCookies()
 
       const { 'next.refreshToken': refreshToken } = cookies
+      const originalConfig = error.config // é basicamente toda a configuração da requisição feita para o back-end (dentro dela tem todas as configurações necessarias para repetir a requisição para o back)
 
-      api.post('/refresh', {
-        refreshToken,
-      }).then(response => {
-        const { token, refreshToken: newRefreshToken } = response.data
+      if (!isRefreshig) {
+        isRefreshig = true
 
-        setCookie(undefined, 'nextauth.token', token, {
-          maxAge: 60 * 60 * 24 * 30, // 30 days
-          path: '/'
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token, refreshToken: newRefreshToken } = response.data
+  
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, // 30 days
+            path: '/'
+          })
+          setCookie(undefined, 'nextauth.refreshToken', newRefreshToken, {
+            maxAge: 60 * 60 * 24 * 30,
+            path: '/'
+          })
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+          filedRequestsQueue.forEach((request: any) => request.onSucess(token))
+          filedRequestsQueue = []
+        }).catch(error => {
+          filedRequestsQueue.forEach((request: any) => request.onFailure(error))
+          filedRequestsQueue = []
+        }).finally(() => {
+          isRefreshig = false
         })
-        setCookie(undefined, 'nextauth.refreshToken', newRefreshToken, {
-          maxAge: 60 * 60 * 24 * 30,
-          path: '/'
-        })
+      }
 
-        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+      return new Promise((resolver, reject) => {
+        filedRequestsQueue.push({
+          onSucess: (token: string) => { // o que acontece quando o processo de refreshToken finaliza
+            originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+            resolver(api(origin))
+          }, 
+            
+          onFailure: (error: AxiosError) => { // o que acontece quando quando o processo falaha
+            reject(error)
+          } 
+            
+        })
       })
     } else {
       // desloga usuário
